1fb8df0e7c4e3e5030f28c399104649e58b8f50b
mk$weety <lupica.francy06@gmail.com>
2025-03-01 18:54:34 +0100

    Comment out unused SQL execution methods in SQLConnection.java for future reference

--- src/main/java/dev/sweety/api/SQLConnection.java
+++ src/main/java/dev/sweety/api/SQLConnection.java
@@ -79,4 +79,61 @@ public interface SQLConnection {
         }
     }
 
+
+    /*
+
+    public <T> T execute(String query, StatementConsumer<T> function) {
+        try (PreparedStatement statement = connection.prepareStatement(query)) {
+            return function.accept(statement);
+        } catch (SQLException e) {
+            e.printStackTrace(System.err);
+        }
+
+        return null;
+    }
+
+    public <T> CompletableFuture<T> executeAsync(String query, StatementConsumer<T> function) {
+        return CompletableFuture.supplyAsync(() -> execute(query, function), getExecutor());
+    }
+
+    public ResultSet executeQuery(String query, Object... params) {
+        try (PreparedStatement statement = connection.prepareStatement(query)) {
+            setParameters(statement, params);
+
+            return statement.executeQuery();
+        } catch (SQLException e) {
+            e.printStackTrace(System.err);
+        }
+
+        return null;
+    }
+
+    public CompletableFuture<ResultSet> executeQueryAsync(String query, Object... params) {
+        return CompletableFuture.supplyAsync(() -> executeQuery(query, params), getExecutor());
+    }
+
+    public int executeUpdate(String query, Object... params) {
+        try (PreparedStatement statement = connection.prepareStatement(query)) {
+            setParameters(statement, params);
+
+            return statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace(System.err);
+        }
+
+        return -1;
+    }
+
+    public CompletableFuture<Integer> executeUpdateAsync(String query, Object... params) {
+        return CompletableFuture.supplyAsync(() -> executeUpdate(query, params), getExecutor());
+    }
+
+    private void setParameters(PreparedStatement statement, Object[] params) throws SQLException {
+        for (int i = 0; i < params.length; i++) {
+            statement.setObject(i + 1, params[i]);
+        }
+    }
+
+    * */
+
 }
